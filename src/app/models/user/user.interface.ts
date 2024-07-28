fdf66ab665d56e083917f832cbecef792e66ddf9
imran0dot <imran0dot@gmail.com>
2024-07-28 11:38:33 +0600

    user interface created

---
+++ src/app/models/user/user.interface.ts
@@ -0,0 +1,8 @@
+export interface Tuser {
+    id: string,
+    password: string,
+    needsPasswordChange: boolean,
+    status: 'in-progress' | 'blocked',
+    role: 'admin' | 'student' | 'teacher',
+    isDeleted: boolean,
+}
