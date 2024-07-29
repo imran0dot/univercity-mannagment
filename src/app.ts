db153db4c94efecf33774e0c814480db665ce011
imran0dot <imran0dot@gmail.com>
2024-07-29 10:48:19 +0600

    no route found api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
 import { userRouts } from './app/models/user/user.route';
 import { StudentRoutes } from './app/models/student/student.rout';
 import { TeacherRouter } from './app/models/teacher/teacher.route';
+import sendResponse from './app/utils/sendResponse';
+import httpStatus from 'http-status';
 
 const app = express();
 
@@ -19,5 +21,15 @@ app.use('/api/v1/students', StudentRoutes)
 app.use('/api/v1/users', userRouts)
 app.use('api/v1/teacher', TeacherRouter);
 
+
+//not found route
+app.get('*', (req: Request, res: Response) => {
+    sendResponse(res, {
+        success: false,
+        data: '',
+        message: 'no route found',
+        statusCode: 404,
+    })
+})
 
 export default app;
